ca4d71b6bfc89b9cb9bb61d3bdc43f62ce6206b9
ksmles36 <ksmles36@naver.com>
2022-10-01 22:57:04 +0900

    Initial commit

---
+++ src/main/java/com/ksm/untitled_byksm/UntitledByKsmApplication.java
@@ -0,0 +1,13 @@
+package com.ksm.untitled_byksm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledByKsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntitledByKsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksm/untitled_byksm/UntitledByKsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm.untitled_byksm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledByKsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
